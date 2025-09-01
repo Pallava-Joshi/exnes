7c8baf9998af3943c1095b77668709f00ba95c7c
Pallava-Joshi <joshipallava@gmail.com>
2025-09-01 19:02:50 +0530

    feat/engine

--- apps/api/src/engine.ts
+++ apps/api/src/engine.ts
@@ -4,6 +4,13 @@ import { prismaClient } from "@repo/db/prisma";
 async function fetchLatestOrders() {
   const orders = await prismaClient.order.findMany({
     where: { status: "OPEN" },
+    include: {
+      user: {
+        include: {
+          balance: true,
+        },
+      },
+    },
   });
   if (orders.length === 0) console.log("no orders yet");
   return orders;
@@ -30,7 +37,30 @@ function isMarginCall(PnL: number, margin: number): boolean {
   return false;
 }
 
-async function closeOrder() {}
+async function closeOrder(PnL: number, processingOrder: any) {
+  const updatedOrder = await prismaClient.order.update({
+    where: {
+      orderId: processingOrder.orderId,
+    },
+    data: {
+      finalPnL: PnL,
+    },
+  });
+  // const processingUser = await prismaClient.user.
+  const calculatedBalance =
+    processingOrder.user.balance.balance + processingOrder.margin + PnL;
+  const updatedBalance = await prismaClient.balance.update({
+    where: {
+      id: processingOrder.userId,
+    },
+    data: {
+      balance: calculatedBalance,
+    },
+  });
+  return { updatedBalance, updatedOrder };
+}
+
+async function liquidator() {}
 //immediate invoked funtion expression => calls itself on start
 (async function tick() {
   const channels = ["trade-data"];
@@ -47,7 +77,6 @@ async function closeOrder() {}
     const latestOrders = await fetchLatestOrders();
     const parsedData = JSON.parse(message);
     const { p: currentPrice, s: token } = parsedData;
-    const {} = latestOrders;
 
     const relevantOrders = latestOrders.filter(
       (order) => order.asset === token
@@ -58,27 +87,18 @@ async function closeOrder() {}
       return;
     }
     for (const processingOrder of latestOrders) {
-      const {
-        buyPrice,
-        qty,
-        leverage,
-        margin,
-        stopLoss,
-        takeProfit,
-        orderType,
-      } = processingOrder;
+      const { buyPrice, qty, margin, stopLoss, takeProfit, orderType } =
+        processingOrder;
       //   if (token !== asset) continue;
-      const PnL =
+      const PnL: number =
         orderType === "LONG"
-          ? (currentPrice - buyPrice.toNumber()) * qty.toNumber() * leverage
-          : (buyPrice.toNumber() - currentPrice) * qty.toNumber() * leverage;
-      if (!stopLoss || !takeProfit) return;
-      if (
-        isStopLoss(PnL, stopLoss.toNumber()) ||
-        isTakeProfit(PnL, takeProfit.toNumber()) ||
-        isMarginCall(PnL, margin.toNumber())
-      ) {
-        await closeOrder();
+          ? (currentPrice - buyPrice.toNumber()) * qty.toNumber()
+          : (buyPrice.toNumber() - currentPrice) * qty.toNumber();
+      const b1 = stopLoss ? isStopLoss(PnL, stopLoss.toNumber()) : false;
+      const b2 = takeProfit ? isTakeProfit(PnL, takeProfit.toNumber()) : false;
+
+      if (isMarginCall(PnL, margin.toNumber()) || b1 || b2) {
+        closeOrder(PnL, processingOrder);
       }
     }
   });
